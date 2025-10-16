fcd583fe35762aaec70962eb8172113ba23a72f5
Edsel <edselreeve.david@msunaawan.edu.ph>
2025-10-16 18:46:26 +0800

    creating CRUD in the student app

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api.views import function_based_view
 
 urlpatterns = [
-    path('fbv-students/', function_based_view.studentView)
+    path('fbv-students/', function_based_view.studentView),
+    path('fbv-student/<int:student_id>', function_based_view.student)
 ]
--- api/views/function_based_view.py
+++ api/views/function_based_view.py
@@ -7,10 +7,9 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def studentView(request):
     if(request.method == 'GET'):
-        # Get all the data from the Student table
         students = Students.objects.all()
         serializer = StudentSerializer(students, many=True)
         print(students, serializer.data)
@@ -22,3 +21,44 @@ def studentView(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if(request.method == 'PUT'):
+        id = request.data.get('id')
+        try:
+            student = Students.objects.get(id=id)
+        except Students.DoesNotExist:
+            return Response({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if(request.method == 'DELETE'):
+        id = request.data.get('id')
+        try:
+            student = Students.objects.get(id=id)
+        except Students.DoesNotExist:
+            return Response({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+        student.delete()
+        return Response({'message': 'Student deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def student(request, student_id):
+    try:
+        student = Students.objects.get(id=student_id)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
